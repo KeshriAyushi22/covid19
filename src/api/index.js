00c2df58e5fd95cf130b949c33d595f4828f20dc
AyushiKeshri <keshri.ayushi10@gmail.com>
2020-05-25 14:50:38 +0530

    chart

--- src/api/index.js
+++ src/api/index.js
@@ -15,7 +15,23 @@ try{
 export const fetchDailyData= async()=>{
     try{
         const {data}= await axios.get(`${url}/daily`);
-     console.log(data)
+         const modifiedData= data.map((dailyData)=>({
+             //returning object
+             confirmed:dailyData.confirmed.total,
+             deaths:dailyData.deaths.toatl,
+             date:dailyData.reportDate
+         }));
+
+         return modifiedData
+    }
+    catch(error){
+        console.log(error);
+    }
+}
+
+export const countries= async()=>{
+    try{
+        const res= await axios.get(`${url}/countries`)
     }
     catch(error){
         console.log(error);
--- src/components/Chart/Chart.jsx
+++ src/components/Chart/Chart.jsx
@@ -1,9 +1,50 @@
-import React,{useState,useEffect} from 'react';
+import React, { useState, useEffect } from 'react';
+import { fetchDailyData } from "../../api";
+import {Line,Bar} from "react-chartjs-2";
+import styles from "./Chart.module.css"
 
-const Chart =()=>{
-    const [dailyData,setDailyData]= useState({});
-    return (
-        <h1>Chart</h1>
+
+const Chart = () => {
+    const [dailyData, setDailyData] = useState({});
+    /** we cant use async with use effect as it takes pure
+     * function as a parameter.
+     * so to call api inside it we can use 
+     * async function and then awaits for its resp and set it 
+     * 
+     */
+    useEffect(() => {
+//will continue hitting the api -wont stop
+        const fetchAPI = async () => {
+            setDailyData(await fetchDailyData());
+        }
+        fetchAPI();
+    })
+
+    const lineChart=(
+        dailyData.length
+        ? (<Line 
+         data={{
+             labels:dailyData.map(({date})=>date),
+             datasets:[{
+                 data:dailyData.map(({confirmed})=>confirmed),
+                 label:'Infected',
+                 borderColor:'#3333ff',
+                 fill:true,
+             },{
+                data:dailyData.map(({confirmed})=>confirmed),
+                label:'Deaths',
+                borderColor:'#3333ff',
+                backgroundColor:'red',
+                fill:true,
+             }]
+         }}/>)
+         :null
+        );
+    
+        return (
+      <div className={styles.container}>
+          {lineChart}
+      </div>
     )
 }
 
